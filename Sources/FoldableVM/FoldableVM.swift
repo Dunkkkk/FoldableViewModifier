0ead5aa4cb91aa9f9a121614414b4a46187317de
pccommen <pccommen@gmail.com>
2023-08-21 16:20:30 +0900

    Let's Foldable

--- Sources/FoldableVM/FoldableVM.swift
+++ Sources/FoldableVM/FoldableVM.swift
@@ -1,2 +1,9 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public extension View {
+    
+    @inlinable
+    func foldable<Content: View>(modifiers: (Self) -> Content) -> Content {
+        modifiers(self)
+    }
+}
