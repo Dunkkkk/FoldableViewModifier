b8844cf45f77aed21da1c4dce02ea81ed3300b9c
pccommen <pccommen@gmail.com>
2023-08-22 14:39:57 +0900

    Let's Foldable

--- Sources/FoldableVM/FoldableVM.swift
+++ Sources/FoldableVM/FoldableVM.swift
@@ -3,7 +3,7 @@ import SwiftUI
 public extension View {
     
     @inlinable
-    func foldable<Content: View>(modifiers: (Self) -> Content) -> Content {
-        modifiers(self)
+    func foldable<Content: View>(modifiers: (Self) throws -> Content) rethrows -> Content {
+        try modifiers(self)
     }
 }
